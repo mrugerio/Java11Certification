0a990d37b09e4e41f1fb556fb897995addea8f8d
Miguel Rugerio <mrugerio@gmail.com>
2022-06-22 10:22:51 -0500

    Create Pregunta16.java

---
+++ chapter03/src/chapter03/Pregunta16.java
@@ -0,0 +1,13 @@
+package chapter03;
+
+public class Pregunta16 {
+
+	public static void main(String[] args) {
+
+		int note = 5;
+		short melody = (byte)(double)(note *= 2);
+		
+		System.out.println(melody);
+	}
+
+}
