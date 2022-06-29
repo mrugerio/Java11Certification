02cd7a1563145f73cfc618a69fceedd68460966f
Miguel Rugerio <mrugerio@gmail.com>
2022-06-29 15:34:48 -0500

    Create Pre01.java

---
+++ chapter05/src/chapter05/Pre01.java
@@ -0,0 +1,22 @@
+package chapter05;
+
+public class Pre01 {
+
+	public static void main(String[] args) {
+		int numFish = 4; 
+		String anotherFish = ""+ numFish + 1  ; //41
+		//System.out.println(anotherFish);
+		
+		
+		String numbers = "012345678";
+
+		System.out.println(numbers.substring(1, 3)); //12
+		System.out.print(numbers.substring(7, 7)); //Nada
+		System.out.println(numbers.substring(7));
+		
+		int[][] types = new int[5][];
+		
+		// avaJ
+	}
+
+}
